bfb39363287a86388e7a18779ccaa25b2f0613b3
eclox.yt@gmail.com <eclox.yt@gmail.com>
2024-10-30 20:25:29 +0800

    comit

--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,21 @@ import { useEffect, useState } from "react";
 const Page = () => {
   const [ blog, setBlog ] = useState([]);
   const [ page, setPage ] = useState(1);
+  const [ loading, setLoading ] = useState(true);
   const router = useRouter();
+
+  {loading ? <p>Loading...</p> : blog.map((item) => (
+    <div key={item.id}>
+      <Items data={item} />
+    </div>
+  ))}
+
 
   const fetchData = async () => {
     const rawData = await fetch(`https://dev.to/api/articles?per_page=9&page=${page}`);
     const data = await rawData.json();
+    setLoading(true);
+    setLoading(false);
     setBlog(data);
   }
   console.log(blog);
@@ -27,8 +37,11 @@ const Page = () => {
   }
 
   const handleInputValueNegative = () => {
-    setPage(page - 1)
-  }
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+  
 
   useEffect(() => {
     fetchData();
@@ -39,16 +52,16 @@ const Page = () => {
       <div className="navbar">
         <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" className="icon"/>
         <div className="nav-stuff">
-          <div className="nav-about-us">About Us</div>
           <div className="nav-contact">Contact</div>
-          <img src="https://www.svgrepo.com/show/343494/profile-user-account.svg" className="user-icon" onClick={handleClick}/>
+          <div className="nav-about-us">About Us</div>
         </div>
+          <img src="https://www.svgrepo.com/show/343494/profile-user-account.svg" className="user-icon" onClick={handleClick}/>
       </div>
         <div className="page-center">
           {blog.map((item, index) => {
             return (
               <div>
-                <div key={index}>
+                <div key={item.id}>
                 <Items data={item} />
                 </div>
               </div>
